ad46d6066c4d329dc6df5ec5182eb4ea77d66be5
Your Name <your.email@example.com>
2025-09-17 17:06:24 +0330

    feat: add `routing.ts` comments

--- src/i18n/routing.ts
+++ src/i18n/routing.ts
@@ -4,5 +4,5 @@ import { defineRouting } from "next-intl/routing";
 export const routing = defineRouting({
   locales: LOCALES,
   defaultLocale: DEFAULT_LOCALE,
-  localePrefix: "as-needed",
+  localePrefix: "as-needed", // or "always", "never"
 });
