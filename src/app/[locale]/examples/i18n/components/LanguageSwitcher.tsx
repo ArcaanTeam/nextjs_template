63cccf3f5ee0a155e61182defdbe4033faffcbc1
Your Name <your.email@example.com>
2025-09-17 16:39:56 +0330

    feat: add `/examples/i18n` page

--- src/app/[locale]/examples/i18n/components/LanguageSwitcher.tsx
+++ src/app/[locale]/examples/i18n/components/LanguageSwitcher.tsx
@@ -1,32 +1,24 @@
 "use client";
 
-import { LOCALES } from "@/config/i18n"; // or wherever you define ["fa", "en"]
+import { LOCALES } from "@/config/i18n";
 import useI18n from "@/hooks/useI18n";
 
 export default function LanguageSwitcher() {
-  const { switchLanguage, getCurrentLocale, getSavedLocale } = useI18n();
-  const savedLocale = getSavedLocale();
-  const currentLocale = getCurrentLocale();
-  console.log("Saved locale:", savedLocale);
-  console.log("Current locale (URL):", currentLocale);
+  const { changeLocale, locale } = useI18n();
 
   return (
     <div className="flex gap-2 p-2 bg-gray-100 rounded">
-      {LOCALES.map((locale) => {
-        const isSavedLocale = locale == savedLocale;
-        console.log(locale, savedLocale, isSavedLocale);
-        return (
-          <button
-            key={locale}
-            onClick={() => switchLanguage(locale)}
-            className={`px-3 py-1 rounded hover:bg-gray-300 transition ${
-              isSavedLocale ? "bg-cyan-400" : ""
-            }`}
-          >
-            {locale}
-          </button>
-        );
-      })}
+      {LOCALES.map(($locale) => (
+        <button
+          key={$locale}
+          className={`px-3 py-1 rounded hover:bg-gray-300 transition ${
+            locale === $locale && "bg-cyan-300"
+          }`}
+          onClick={() => changeLocale($locale)}
+        >
+          {$locale}
+        </button>
+      ))}
     </div>
   );
 }
--- src/app/[locale]/examples/i18n/page.tsx
+++ src/app/[locale]/examples/i18n/page.tsx
@@ -1,9 +1,15 @@
+"use client";
+
+import { useTranslations } from "next-intl";
 import LanguageSwitcher from "./components/LanguageSwitcher";
 
 export default function Page() {
+  const t = useTranslations();
+
   return (
     <div>
       <LanguageSwitcher />
+      <h1>{t("welcome")}</h1>
     </div>
   );
 }
