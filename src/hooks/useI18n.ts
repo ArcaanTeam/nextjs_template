abae9edb66666267ce7e49d7d2213589fc557ea6
Your Name <your.email@example.com>
2025-09-17 16:39:34 +0330

    feat: useI18n hook

--- src/hooks/useI18n.ts
+++ src/hooks/useI18n.ts
@@ -1,61 +1,87 @@
-import {
-  DEFAULT_LOCALE,
-  isLocaleStringValid,
-  LOCALE_REGEX,
-  ValidLocaleString,
-} from "@/config/i18n";
+import { DEFAULT_LOCALE, isLocaleStringValid } from "@/config/i18n";
 import { Key } from "@/constants/enums";
 import { SET_LOCALE_COOKIE_CONFIG } from "@/constants/frequents";
-import { getCookieClient, setCookieClient } from "@/utils/cookie.utils";
+import { routing } from "@/i18n/routing";
+import { getCookieClient, setCookieClient } from "@/utils/cookie";
+import { useLocale } from "next-intl";
 import { usePathname, useRouter } from "next/navigation";
+import { useCallback, useState } from "react";
 
 export default function useI18n() {
   const router = useRouter();
   const pathname = usePathname();
+  const local = useLocale();
 
-  function switchLanguage(newLocale: ValidLocaleString) {
-    // Set cookie for middleware to read
-    setCookieClient(
-      Key.CookieI18nSavedLocale,
-      `${newLocale}`,
-      SET_LOCALE_COOKIE_CONFIG
-    );
+  /**
+   * Store the locale as state so that components re-render immediately
+   * after a switch, without waiting for Router events
+   */
+  const [locale, setLocale] = useState(local);
 
-    let newPathname: string;
+  const changeLocale = useCallback(
+    (newLocale: string) => {
+      if (!isLocaleStringValid(newLocale) || newLocale === locale) return;
 
-    if (newLocale === DEFAULT_LOCALE) {
-      newPathname = pathname.replace(LOCALE_REGEX, ""); // remove any locale
-      router.push(newPathname || "/"); // fallback to root
+      if (typeof window !== "undefined") {
+        setCookieClient(
+          Key.CookieI18nSavedLocale,
+          newLocale,
+          SET_LOCALE_COOKIE_CONFIG
+        );
+      }
+
+      setLocale(newLocale);
+      pushToNewUrl(newLocale);
+    },
+    [locale, router]
+  );
+
+  function pushToNewUrl(newLocale: string) {
+    const localePrefixCondition = routing.localePrefix as String;
+    const currentUrl = new URL(window.location.href);
+    const currentLocale = getLocaleFromUrl();
+    let newPath: string;
+
+    const newLocalePrefix =
+      localePrefixCondition === "always"
+        ? newLocale
+        : localePrefixCondition === "never"
+        ? ""
+        : routing.localePrefix === "as-needed"
+        ? newLocale === DEFAULT_LOCALE
+          ? ""
+          : newLocale
+        : "";
+
+    if (currentLocale === DEFAULT_LOCALE) {
+      newPath = newLocalePrefix.concat(pathname);
     } else {
-      // If switching FROM default (no prefix) → add new locale
-      // Or replace existing non-default locale
-      newPathname = pathname.replace(LOCALE_REGEX, `/${newLocale}`);
-      router.push(newPathname);
+      newPath = pathname.replace(`/${currentLocale}`, newLocalePrefix);
     }
+
+    currentUrl.pathname = newPath;
+    const finalUrl = currentUrl.toString();
+    router.replace(finalUrl);
   }
 
   function getSavedLocale() {
     return getCookieClient(Key.CookieI18nSavedLocale);
   }
 
-  function getCurrentLocale() {
+  function getLocaleFromUrl() {
     const supposeToBeLocaleString = pathname.split("/")[1];
     const isValid = isLocaleStringValid(supposeToBeLocaleString);
     if (isValid) {
       return supposeToBeLocaleString;
     } else {
-      const savedLocale = getSavedLocale();
-      if (savedLocale === DEFAULT_LOCALE) {
-        return savedLocale;
-      } else {
-        return ""; // Undefined condition
-      }
+      return DEFAULT_LOCALE;
     }
   }
 
   return {
-    switchLanguage,
+    changeLocale,
     getSavedLocale,
-    getCurrentLocale,
+    getLocaleFromUrl,
+    locale,
   };
 }
