d91eb8a70347325971c9a537f0e04daff5423b57
Your Name <your.email@example.com>
2025-09-24 17:25:50 +0330

    Add basic variants

---
+++ src/app/examples/shadcn/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@/ui/components/button";
+
+export default function Page() {
+  return (
+    <div className="w-full h-full p-10 flex flex-col gap-5">
+      <Button variant="brand">Click</Button>
+    </div>
+  );
+}
---
+++ src/ui/config/variants.ts
@@ -0,0 +1,43 @@
+import { cva } from "class-variance-authority";
+const globalVariants = {
+  warning:
+    "bg-warning text-warning-foreground hover:bg-warning/90 active:bg-warning/100 focus-visible:ring-warning/20 dark:focus-visible:ring-warning/40",
+  success:
+    "bg-success text-success-foreground hover:bg-success/90 active:bg-success/100 focus-visible:ring-success/20 dark:focus-visible:ring-success/40",
+  info: "bg-info text-info-foreground hover:bg-info/90 active:bg-info/100 focus-visible:ring-info/20 dark:focus-visible:ring-info/40",
+  error:
+    "bg-error text-error-foreground hover:bg-error/90 active:bg-error/100 focus-visible:ring-error/20 dark:focus-visible:ring-error/40",
+  brand:
+    "bg-brand text-brand-foreground hover:bg-brand/90 active:bg-brand/100 focus-visible:ring-brand/20 dark:focus-visible:ring-brand/40",
+};
+
+export const buttonVariants = cva(
+  "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
+  {
+    variants: {
+      variant: {
+        default: "bg-primary text-primary-foreground hover:bg-primary/90",
+        destructive:
+          "bg-destructive text-white hover:bg-destructive/90 focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40 dark:bg-destructive/60",
+        outline:
+          "border bg-background shadow-xs hover:bg-accent hover:text-accent-foreground dark:bg-input/30 dark:border-input dark:hover:bg-input/50",
+        secondary:
+          "bg-secondary text-secondary-foreground hover:bg-secondary/80",
+        ghost:
+          "hover:bg-accent hover:text-accent-foreground dark:hover:bg-accent/50",
+        link: "text-primary underline-offset-4 hover:underline",
+        ...globalVariants,
+      },
+      size: {
+        default: "h-9 px-4 py-2 has-[>svg]:px-3",
+        sm: "h-8 rounded-md gap-1.5 px-3 has-[>svg]:px-2.5",
+        lg: "h-10 rounded-md px-6 has-[>svg]:px-4",
+        icon: "size-9",
+      },
+    },
+    defaultVariants: {
+      variant: "default",
+      size: "default",
+    },
+  }
+);
